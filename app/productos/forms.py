129d06ed2baac77d341659a4b3a226774f597163
Cesar Fernando Montealegre Correa <cesarmontealegere@gmail.com>
2023-09-12 12:21:47 -0500

    Delete

--- app/productos/forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-
-class RegistrarProductoForm(FlaskForm):
-    nombre = StringField("Nombre del producto:")
-    precio = StringField("Precio del producto:")
-    submit = SubmitField("Guardar")
